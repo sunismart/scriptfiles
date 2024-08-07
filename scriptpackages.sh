78d83d083f3205e253a74b0eb3f29e7e3b3734ca
root <root@ip-172-31-45-127.ap-south-1.compute.internal>
2024-08-07 15:42:27 +0000

    feature committed

--- scriptpackages.sh
+++ scriptpackages.sh
@@ -31,8 +31,3 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/s
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install -y terraform
 
-# Verify Terraform installation
-terraform -version
-
-
-
